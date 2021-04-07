f7f4c9c5698afcb147163b80b6bc3e473f1579c6
Vlad-Orlov <orlov.vlad.serg@gmail.com>
2021-04-07 17:19:10 +0200

    added printing of event number

--- src/OpNoviceEventAction.cc
+++ src/OpNoviceEventAction.cc
@@ -56,6 +56,10 @@ OpNoviceEventAction::OpNoviceEventAction()
 
 void OpNoviceEventAction::BeginOfEventAction( const G4Event*)
 { 
+
+  G4cout << "\n---> Begin of Event: " << G4RunManager::GetRunManager()->
+  GetCurrentEvent()->GetEventID() << G4endl;
+
   scintillation_photons = 0;
   cherenkov_photons = 0;
 }
