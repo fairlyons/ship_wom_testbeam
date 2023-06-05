e102824b79d323f9a75dddfb9bd1aa64cbe0f1a5
fairlyons <fair.lyons@gmail.com>
2023-06-05 15:23:06 +0200

    updated analysis script

--- cell_ana.C
+++ cell_ana.C
@@ -5,75 +5,35 @@ void cell_ana(TString f) {
     chain->Add(f);
 
     TTreeReader *data = new TTreeReader(chain);
-    TTreeReaderValue<Double_t>   x(*data,"row_wise_branch.x");
-    TTreeReaderValue<Double_t>   y(*data,"row_wise_branch.y");
-    TTreeReaderValue<Int_t>   wom(*data,"row_wise_branch.WOMnumber");
-    TTreeReaderValue<Double_t>   time(*data,"row_wise_branch.time");
-    TTreeReaderValue<Int_t>   eventN(*data,"row_wise_branch.eventNumber");
+    TTreeReaderValue<Int_t>      wom(*data,"sipmNumber");
+    TTreeReaderValue<Double_t>   time(*data,"time");
+    TTreeReaderValue<Int_t>      eventN(*data,"eventNumber");
 
     int entries = chain->GetEntries();
-    int events = 10;
+    int events = 1;
 
-    TH2D *h1 = new TH2D("", "", 1000, -400, 400, 1000, -600, 600);
-    h1->GetXaxis()->SetTitle("X (mm)");
-    h1->GetYaxis()->SetTitle("Y (mm)");
+    TH1I *h1 = new TH1I("", "", 16, 0, 16);
+    h1->GetXaxis()->SetTitle("SiPM Number");
+    h1->GetYaxis()->SetTitle("Events");
 
-    TH1I *h2 = new TH1I("", "", 2, 1, 3);
-    h2->GetXaxis()->SetTitle("WOM Number");
+    TH1D *h2 = new TH1D("", "", 100, 0, 1000);
+    h2->GetXaxis()->SetTitle("Time (ns)");
     h2->GetYaxis()->SetTitle("Events");
 
-    TH1D *h3 = new TH1D("", "", 100, 0, 1000);
-    h3->GetXaxis()->SetTitle("Time (ns)");
-    h3->GetYaxis()->SetTitle("Events");
-
-    double N[events], w1[events], w2[events];
+    double N[events], up[events], down[events];
     for(int i = 0; i < events; i++) N[i] = i+1;
 
     while (data->Next()) {
-      h1->Fill(*x,*y);
-      h2->Fill(*wom);
-      h3->Fill(*time);
-      if(*wom == 1) w1[*eventN]++;
-      if(*wom == 2) w2[*eventN]++;
+      h1->Fill(*wom);
+      h2->Fill(*time);
+      if(*wom < 8) down[*eventN]++;
+      if(*wom > 7) up[*eventN]++;
     }
 
-    ifstream fin("b/run1.mac");
-    string s;
-    s.reserve(50);
-    string pos[2], dir[2];
-    for(int i = 0; i < 2; ++i) std::getline(fin, s);
-    std::getline(fin,s);
-    int j = 14;
-    for(int i = j; i < s.size(); ++i) {
-      j++;
-      if(s[i] == ' ') break;
-      pos[0].push_back(s[i]);
-    }
-    for(int i = j; i < s.size(); ++i) {
-      j++;
-      if(s[i] == ' ') break;
-      pos[1].push_back(s[i]);
-    }
-    std::getline(fin,s);
-    j = 15;
-    for(int i = j; i < s.size(); ++i) {
-      j++;
-      if(s[i] == ' ') break;
-      dir[0].push_back(s[i]);
-    }
-    for(int i = j; i < s.size(); ++i) {
-      j++;
-      if(s[i] == ' ') break;
-      dir[1].push_back(s[i]);
-    }
-
-    TCanvas *c1 = new TCanvas("c1","c1",10,10,800,1000);
-    TLine* track = new TLine(stoi(pos[0]), stoi(pos[1]), stoi(pos[0]) + stoi(dir[0]) * 5000, stoi(pos[1]) + stoi(dir[1]) * 5000);
-    track->SetLineColor(kRed);
-    c1->SetLeftMargin(0.15);
-    h1->SetStats(0);
-    h1->Draw("COLZ");
-    track->Draw("SAME");
+    TCanvas *c1 = new TCanvas("c1","c1",10,10,1200,800);
+    //gPad->SetLogy();
+    h1->SetLineColor(kBlue);
+    h1->Draw("HIST");
 
     TCanvas *c2 = new TCanvas("c2","c2",10,10,1200,800);
     //gPad->SetLogy();
@@ -81,26 +41,21 @@ void cell_ana(TString f) {
     h2->Draw("HIST");
 
     TCanvas *c3 = new TCanvas("c3","c3",10,10,1200,800);
-    gPad->SetLogy();
-    h3->SetLineColor(kBlue);
-    h3->Draw("HIST");
-
-    TCanvas *c4 = new TCanvas("c4","c4",10,10,1200,800);
-    c4->Divide(1,2);
-    c4->cd(1);
-    TGraph *h4 = new TGraph(events, N, w2);
+    c3->Divide(1,2);
+    c3->cd(1);
+    TGraph *h3 = new TGraph(events, N, up);
+    h3->GetXaxis()->SetTitle("Event");
+    h3->GetYaxis()->SetTitle("Detected photons in Upper WOM");
+    //gPad->SetLogy();
+    h3->SetFillColor(38);
+    h3->Draw("AB");
+    c3->cd(2);
+    TGraph *h4 = new TGraph(events, N, down);
     h4->GetXaxis()->SetTitle("Event");
-    h4->GetYaxis()->SetTitle("Detected photons in Upper WOM");
+    h4->GetYaxis()->SetTitle("Detected photons in Lower WOM");
     //gPad->SetLogy();
     h4->SetFillColor(38);
     h4->Draw("AB");
-    c4->cd(2);
-    TGraph *h5 = new TGraph(events, N, w1);
-    h5->GetXaxis()->SetTitle("Event");
-    h5->GetYaxis()->SetTitle("Detected photons in Lower WOM");
-    //gPad->SetLogy();
-    h5->SetFillColor(38);
-    h5->Draw("AB");
 
     return;
 }
